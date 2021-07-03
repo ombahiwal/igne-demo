2259105c97dd130e7901c76e8a7aa8be6068692e
ombahiwal@gmail.com <ombahiwal@gmail.com>
2021-07-03 23:44:00 +0530

    clean up -script-deploy

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import img1 from './components/sample_images/image1.jpg'
 import img2 from './components/sample_images/image2.jpg'
 import img3 from './components/sample_images/image3.jpg'
 import img4 from './components/sample_images/image4.jpg'
+
 function App() {
   // Sample Images 
   const carousel_image_paths = [img1, img2, img3, img4];
--- src/components/Plate.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'; 
-
-const Plate = props=>{
-    return <input type="text"/>
-}
-
-export default Plate;
